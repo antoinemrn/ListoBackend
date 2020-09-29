1e3a5f50a71a40dd99826680a035733b41deb1f4
Antoine MARINIER <antoinemarinier@unca.fr>
2020-09-29 17:22:17 +0200

    ADD/GET Task

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const TaskModel = require("./models/task");
+const multer = require("multer");
+const task = require("./models/task");
+var upload = multer();
+
+mongoose
+  .connect(
+    "mongodb+srv://listo_admin:listo_admin190769@cluster0.tgccr.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion Mongo réussie"))
+  .catch(() => console.log("Connexion mongo échouée !"));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -15,27 +29,33 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/stuff", (req, res, next) => {
-  const stuff = [
-    {
-      _id: "oeihfzeoi",
-      title: "Mon premier objet",
-      description: "Les infos de mon premier objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 4900,
-      userId: "qsomihvqios",
-    },
-    {
-      _id: "oeihfzeomoihi",
-      title: "Mon deuxième objet",
-      description: "Les infos de mon deuxième objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 2900,
-      userId: "qsomihvqios",
-    },
-  ];
-  res.status(200).json(stuff);
+app.use(upload.array());
+app.use(express.static("public"));
+
+app.use(bodyParser.json());
+
+app.post("/api/tasks", (req, res, next) => {
+  delete req.body._id;
+  const task = new TaskModel({
+    ...req.body,
+  });
+  console.log(task);
+  task
+    .save()
+    .then(() => res.status(201).json({ message: "Task registered !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.get("/api/tasks/:id", (req, res, next) => {
+  console.log("GET : " + req.params.id);
+  TaskModel.findOne({ _id: req.params.id })
+    .then((task) => res.status(200).json(task))
+    .catch((error) => res.status(404).json({ error }));
+});
+
+app.use("/api/tasks", (req, res, next) => {
+  TaskModel.find()
+    .then((things) => res.status(200).json(things))
+    .catch((error) => res.status(400).json({ error }));
 });
 module.exports = app;
